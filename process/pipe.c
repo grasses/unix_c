d005480de78906abfb8c5b76154de27df32e8ac0
grasses <xiaocao.grasses@gmail.com>
2017-03-21 23:03:55 +0800

    pipe

--- process/pipe.c
+++ process/pipe.c
@@ -4,13 +4,14 @@
 #include <string.h>
 
 int main(void) {
+	// fd[0] = read pipe; fd[1] = write pipe
 	int fd[2], ret;
 	ret = pipe(fd);
 	if (ret < 0) {
 		perror("pipe error\n");
 		exit(1);
 	}
-
+	
 	pid_t pid = fork();
 	if (pid == 0) {
 		printf("=> child\n");
@@ -23,10 +24,10 @@ int main(void) {
 			printf("=> child: read = %s\n", buff);
 		}
 		close(fd[0]);
-
+	
 	} else if (pid > 0) {
 		printf("=> parent\n");
-		
+
 		close(fd[0]); // parent process close read
 		char buff[1024] = "123456789";
 		ret = write(fd[1], buff, strlen(buff));
